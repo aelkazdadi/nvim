3a3c14d4922233ab481753ad63cd2357179d57d7
El Kazdadi <aamr.elkazdadi@gmail.com>
2019-09-23 00:46:08 +0200

    Added packages

--- setup.sh
+++ setup.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
-VENV_DIR=$HOME/.local/share/nvim/venv
+VENV_DIR=$HOME/.local/share/nvim
 
-[ -d $VENV_DIR ] || python3 -m venv --prompt='neovim' $VENV_DIR
-. $VENV_DIR/bin/activate
-$VENV_DIR/bin/pip3 install\
-    appdirs\
-    astroid\
-    attrs\
-    autopep8\
-    black\
-    Click\
-    entrypoints\
-    flake8\
-    greenlet\
-    isort\
-    lazy-object-proxy\
-    mccabe\
-    msgpack\
-    neovim\
-    pycodestyle\
-    pyflakes\
-    pylint\
-    pynvim\
-    six\
-    toml\
-    typed-ast\
-    Unidecode\
-    wrapt\
-    rope\
-    jedi
+[ -d "$VENV_DIR/venv" ] || python3 -m venv --prompt='neovim' "$VENV_DIR/venv"
+$VENV_DIR/venv/bin/pip install\
+  wheel\
+  scikit-build\
+  appdirs\
+  astroid\
+  attrs\
+  autopep8\
+  black\
+  Click\
+  entrypoints\
+  flake8\
+  greenlet\
+  isort\
+  lazy-object-proxy\
+  mccabe\
+  msgpack\
+  neovim\
+  pycodestyle\
+  pyflakes\
+  pylint\
+  pynvim\
+  six\
+  toml\
+  typed-ast\
+  Unidecode\
+  wrapt\
+  rope\
+  jedi
